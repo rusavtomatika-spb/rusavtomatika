ebecae4b9f1ef190423e40f2bacab9efc50a7f24
denis <ddo@rusavtomatika.bizml.ru>
2025-09-23 11:11:35 +0300

    добавил иконку для кнопки 3d модели

--- abacus/components/catalog_detail/templates/default/template.php
+++ abacus/components/catalog_detail/templates/default/template.php
@@ -450,6 +450,11 @@ CoreApplication::include_component( array( "component" => "breadcrumbs" ) );
             $model3dPath = "models/{$cleanModelName}-model/";
           ?>
           <a href="/<?= $model3dPath ?>" class='button' target="_blank" style="text-decoration: none;">
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="16" height="16" viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet" style="margin-right: 5px;">
+              <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
+                <path d="M1450 4481 l-1105 -638 0 -1283 0 -1283 1106 -638 1106 -639 1106 638 1106 637 0 1285 0 1285 -1101 635 c-606 349 -1104 636 -1107 637 -3 2 -503 -285 -1111 -636z m1963 -203 c469 -270 865 -499 880 -508 l28 -17 -873 -504 c-479 -277 -879 -505 -888 -506 -16 -3 -1753 997 -1754 1010 -1 8 1730 1014 1747 1016 5 1 392 -220 860 -491z m-1887 -1296 l879 -507 3 -1018 c1 -559 -1 -1017 -5 -1017 -3 0 -402 228 -885 507 l-878 507 0 1018 c0 560 1 1018 3 1018 2 0 399 -228 883 -508z m2944 -511 l0 -1019 -872 -504 c-479 -276 -875 -505 -880 -506 -4 -2 -8 455 -8 1015 l0 1018 878 507 c482 279 878 507 880 508 1 0 2 -459 2 -1019z"/>
+              </g>
+            </svg>
             3D просмотр
           </a>
         <?php endif; ?>
