b483a73b24323702879de1d87143ae0319aac7c8
denosaur98 <denosaur98@yandex.ru>
2026-02-16 16:59:28 +0300

    fix ui

--- include_utf_8/main_page/content/block_cloud_tags_popup.php
+++ include_utf_8/main_page/content/block_cloud_tags_popup.php
@@ -6,13 +6,13 @@ global $arr_cloud_tags_popup;
 
 ?>
 
-<section class="cloud_tags" style="display: none">
+<section class="cloud_tags" style="display: none; margin: 0 20px; margin-left: 0; margin-right: 0;">
     <div class="cloud_tray top_2rem">
         <div itemscope itemtype="https://www.schema.org/SiteNavigationElement">
 
             <meta itemprop="name" content="Облако тегов">
 
-            <ul class="product_cloud_list cloud_list">
+            <ul class="product_cloud_list cloud_list" style="gap: 10px;">
 
                 <?
 
@@ -22,9 +22,9 @@ global $arr_cloud_tags_popup;
 
                     <li
 
-                            class="product_cloud_item cloud_item">
+                            class="product_cloud_item cloud_item cloud_item_functional">
 
-                        <a itemprop="url" href="<?= $tag['url'] ?>">
+                        <a itemprop="url" href="<?= $tag['url'] ?>" style="text-shadow: none;">
 
                             <?= $tag['name'] ?>
 
