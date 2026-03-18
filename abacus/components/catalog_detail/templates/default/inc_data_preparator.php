610dabf1cc40036762b6b6411245bbca1963dc5c
Maxim <231040864+rusavtomatika-spb@users.noreply.github.com>
2026-03-18 16:19:52 +0300

    правка вывода характеристик товара: "есть" вместо 1

--- abacus/components/catalog_detail/templates/default/inc_data_preparator.php
+++ abacus/components/catalog_detail/templates/default/inc_data_preparator.php
@@ -269,7 +269,18 @@ usort($arSelected_articles, function($a, $b) {
     if (isset($arResult['product']["camera_usb"]) and $arResult['product']["camera_usb"] == 1) {
         $arResult['product']["camera_usb"] = "Есть";
     }
-
+    if (isset($arResult['product']["modbus_support"]) and $arResult['product']["modbus_support"] == "1") {
+        $arResult['product']["modbus_support"] = "Есть";
+    }
+    if (isset($arResult['product']["opc_ua"]) and $arResult['product']["opc_ua"] == "1") {
+        $arResult['product']["opc_ua"] = "Есть";
+    }
+    if (isset($arResult['product']["mqtt"]) and $arResult['product']["mqtt"] == "1") {
+        $arResult['product']["mqtt"] = "Есть";
+    }
+    if (isset($arResult['product']["usb_host"]) and $arResult['product']["usb_host"] == "1") {
+        $arResult['product']["usb_host"] = "Есть";
+    }
 //    if (isset($arResult['product']["hdd_size_gb"]) and $arResult['product']["hdd_size_gb"] == 0) {
 //        $arResult['product']["hdd_size_gb"] = "Отсутствует";
 //    }
