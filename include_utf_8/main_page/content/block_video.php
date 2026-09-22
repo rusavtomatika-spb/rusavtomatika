94be32a470d6bf5e06b166ccac5ad9d00b7674d9
denosaur98 <denosaur98@yandex.ru>
2026-09-22 12:44:57 +0300

    правки верстки: компоненты на Главной

--- include_utf_8/main_page/content/block_video.php
+++ include_utf_8/main_page/content/block_video.php
@@ -1,11 +1,10 @@
-<section class="hor_gallery_block section_video" style="display: none">
-    <h2 class="is-size-3 has-text-weight-medium bright_text">
-        <a class="bright_text" href="/video<?=EX?>/">Видеоканал rusavtomatika.com (70+ обучающих видео)</a>
-    </h2>
-    <?
-    $arguments["component"] = "news_slider";
-    $arguments["template"] = "video";
-    CoreApplication::include_component($arguments);
-    ?>
-
+<section class="hor_gallery_block section_video" style="display: none">
+    <h2 class="is-size-3 has-text-weight-medium bright_text">
+        <a class="bright_text" href="/video<?=EX?>/">Видеоканал rusavtomatika.com <span class="post-text">(70+ обучающих видео)</span></a>
+    </h2>
+    <?
+    $arguments["component"] = "news_slider";
+    $arguments["template"] = "video";
+    CoreApplication::include_component($arguments);
+    ?>
 </section>
--- index.php
+++ index.php
@@ -173,6 +173,7 @@ CoreApplication::add_script("/include_utf_8/main_page/js/cloud.js");
         .double-swiper-block {
             display: flex;
             flex-direction: column;
+            gap: 0;
         }
 
         .double-swiper-block::before {
